0fb76817f3cfa4f03210bd999f241e916c042047
Aaron Huang <q4n0b@ugrad.cs.ubc.ca>
2018-06-11 17:36:51 -0700

    Added deploy script.

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Call: deploy.sh [prod, prev] [version]
+# Eg:   deploy.sh prev 0.02
+
+# Variables
+repo="$HOME/dev/SURFCal"
+source="$repo/web"
+branch="master"
+public="/var/www/html/SURFCal"
+version="0.01"
+
+# Adjustments from parameters.
+if [ ! -z $2 ]; then version=$2; fi
+if [ ! -z $1 ] && [ $1 == "prev" ]; then version=$version"_prev"; fi
+
+# Fetch
+cd $repo
+git checkout $branch
+git pull origin $branch
+
+printf "\nDeploying in 5 seconds..."
+sleep 5
+
+# Update
+sudo rm -rf $public
+sudo cp -r $source $public
+chmod 755 -R $public
+
+# Done
+printf " Done.\n";
