f5124d4d97b91530e63ef735ab3e8ee9cefcccd7
Natalie Ngan <i1y8@ugrad.cs.ubc.ca>
2018-06-13 19:38:43 +0000

    Fixed line break issues with deploy.sh file and moved contact query functions to database.php.

--- web/sources/contact/main.php
+++ web/sources/contact/main.php
@@ -7,14 +7,8 @@ if (basename($_SERVER['PHP_SELF']) === 'main.php') {
 $pageTitle = '';
 
 include('styles/header.php');
-ini_set("log_errors", 1);
-ini_set("error_log", "/tmp/php-error.log");
-error_log( "Php Errors!" );
 
 // Main content.
-
-
-include('functions.php');
 
 getContacts();
 getContactDetails(5);
