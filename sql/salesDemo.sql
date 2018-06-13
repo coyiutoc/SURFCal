b9218fcdbea10d9257e8ea21c3e5a7ea53037d9b
Justin Lee <k4d9@lulu.ugrad.cs.ubc.ca>
2018-06-13 13:55:03 -0700

    initialize salesDemo file

---
+++ sql/salesDemo.sql
@@ -0,0 +1,16 @@
+/* populating tables */
+
+INSERT INTO Calendars VALUES 
+    (null, 'testCalendar', 'calendar for sales demo'),
+    (null, 'testGroupCalendar', 'group calendar for sales demo');
+
+INSERT INTO ItemType VALUES
+    ('event'),
+    ('reminder'),
+    ('note'),
+    ('task');
+
+INSERT INTO Accounts VALUES
+    (),
+    (),
+    ();
