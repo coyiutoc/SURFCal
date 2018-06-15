8d241587c5ec9e984103568ef524375215d035e5
Natalie Ngan <i1y8@ugrad.cs.ubc.ca>
2018-06-15 00:53:33 +0000

    Removed function calls for dev testing.

--- web/sources/calendar/main.php
+++ web/sources/calendar/main.php
@@ -10,11 +10,6 @@ include('styles/header.php');
 
 // Main content.
 
-// Sample function calls for dev testing:
-// getAccountsInCalendar(3);
-// getAllAccounts();
-removeAccountFromCalendar(2, 4);
-
 include('styles/footer.php');
 
 ?>
