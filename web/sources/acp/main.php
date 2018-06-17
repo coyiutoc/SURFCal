8430aa6bc81a8ef4e33689a710b396ed85706056
Aaron Huang <q4n0b@ugrad.cs.ubc.ca>
2018-06-17 12:44:28 -0700

    Base for admin panel.

--- web/sources/acp/main.php
+++ web/sources/acp/main.php
@@ -4,13 +4,40 @@ if (basename($_SERVER['PHP_SELF']) === 'main.php') {
     require_once('../403.php');
 }
 
-$pageTitle = '';
+$pageTitle = 'Site Admin Panel';
 $pageMode = '';
 
-include('styles/header.php');
+$loadMain = false;
 
-// Main content.
-
-include('styles/footer.php');
+if ($admin !== true) {
+    header("Location: ?$profile=404");
+} else {
+    $getSection = isset($_GET['acp']) ? $_GET['acp'] : '';
+    switch ($getSection) {
+        case NULL:
+            case '':
+            header("Location: ?$profile=acp&acp=main");
+            break;
+        case 'main':
+            $loadMain = true;
+            break;
+        case 'accounts':
+            include('sources/acp/accounts.php');
+            break;
+        case 'stats':
+            include('sources/acp/stats.php');
+            break;
+        default:
+            header("Location: ?$profile=404");
+            break;
+    }
+    
+    if ($loadMain === true) {
+        include('styles/header.php');
+        
+        // Main content.        
+        include('styles/footer.php');
+    }
+}
 
 ?>
