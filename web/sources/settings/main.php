63ca29c4c1f2d5d8af976aec430a0a2d1084da4a
juice-tn <jihu102938@gmail.com>
2018-06-18 05:04:18 -0700

    populate form with user info

--- web/sources/settings/main.php
+++ web/sources/settings/main.php
@@ -23,32 +23,32 @@ else
    		<form id="userInfo" action="?$profile=contact&contact=addContact" method="post">
    			<div class="field">
    				<label for="username">Username</label>
-    			<input type="text" name="username" placeholder="Username" required="required" maxlength="64">
+    			<input type="text" name="username" value="$accountInfo[username]" required="required" maxlength="64">
     		</div>
     		<div class="field">
     			<label for="birthday">Birthday</label>
-    			<input type="date" name="birthday" placeholder="birthday" maxlength="64">
+    			<input type="date" name="birthday" value="$accountInfo[birthday]" maxlength="64">
    			</div>
     		<div class="field">
                 <label for="name">Name</label>
-                <input type="text" name="name" placeholder="name" required="required" maxlength="64">
+                <input type="text" name="name" value="$accountInfo[name]" required="required" maxlength="64">
             </div>
     		<div class="field">
     			<label for="email0">Email</label>
-    			<input type="email" name="email0" placeholder="email" required="required" maxlength="64">
+    			<input type="email" name="email0" value="$accountInfo[email]" required="required" maxlength="64">
     		</div>
     		<div>
-    		   <input type="submit" name="updateContact" value="Update Contact" />
+    		   <input type="submit" name="updateContact" value="Update User Info" />
     		</div>
     	</form>
     </aside>
 _END;
-	include('styles/footer.php');
-	// have update button which updates user information (PUT)
 
 	// get calendar information associated with current user
 	// display form with calendar information
 	// have update button which updates calendar information (PUT)
+
+	include('styles/footer.php');
 
 }
 
