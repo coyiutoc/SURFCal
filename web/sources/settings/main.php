ae8a40ff50fc9f7d062a441d7d863671cc0ae916
juice-tn <jihu102938@gmail.com>
2018-06-18 04:51:55 -0700

    form for updating user info

--- web/sources/settings/main.php
+++ web/sources/settings/main.php
@@ -13,16 +13,37 @@ if(!$loggedin){
 else
 {
 	include('styles/header.php');
-	include('styles/footer.php');
 
 	// get information associated with current user
 	$accountInfo = getAccountByUser($user);
-	echo("<script>console.log('PHP: id');</script>");
-        echo "<tr><th>".$accountInfo['username']."</th>";
-        echo "<th>".$accountInfo['email']."</th>";
-        echo "<th>".$accountInfo['name']."</th>";
-        echo "<th>".$accountInfo['birthday']."</th></tr>";
+
 	// display form filled out with user information
+	echo <<< _END
+    	<aside class="userInfo">
+    		<form id="userInfo" action="?$profile=contact&contact=addContact" method="post">
+    			<div class="field">
+    				<label for="username">Username</label>
+    				<input type="text" name="username" placeholder="Username" required="required" maxlength="64">
+    			</div>
+    			<div class="field">
+    				<label for="birthday">Birthday</label>
+    				<input type="date" name="birthday" placeholder="birthday" maxlength="64">
+    			</div>
+    			<div class="field">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" placeholder="name" required="required" maxlength="64">
+                </div>
+    			<div class="field">
+    				<label for="email0">Email</label>
+    				<input type="email" name="email0" placeholder="email" required="required" maxlength="64">
+    			</div>
+    			<div>
+    		      <input type="submit" name="updateContact" value="Update Contact" />
+    		    </div>
+    	    </form>
+    	</aside>
+    _END;
+	include('styles/footer.php');
 	// have update button which updates user information (PUT)
 
 	// get calendar information associated with current user
