605c9f79f6c2648eddb31c775c941c56bcc900d6
Caitlin Coyiuto <t2b1b@ugrad.cs.ubc.ca>
2018-06-18 05:33:24 +0000

    Update + delete calendar items.

--- web/config/database.php
+++ web/config/database.php
@@ -2,6 +2,7 @@
 
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 if (basename($_SERVER['PHP_SELF']) === 'database.php') {
     require_once('../403.php');
@@ -146,7 +147,7 @@ function updateCalendar($accountId, $calendarId, array $args = array()){
         echo("RESULTING QUERY: " . $query); 
         mysqli_query($conn, $query);
 
-        return checkUpdateSuccess($conn);
+        return checkUpdateSuccess($conn, $query);
     }
     else{
         echo "<br> !!!!!! COULD NOT Update Calendar with CalendarId = " . $calendarId . " !!!!!! <br>";
@@ -212,7 +213,7 @@ function deleteCalendar($accountId, $calendarId){
         echo("RESULTING QUERY: " . $query); 
         mysqli_query($conn, $query);
 
-        return checkUpdateSuccess($conn);
+        return checkUpdateSuccess($conn, $query);
     }
     else{
         echo "<br> !!!!!! COULD NOT Delete Calendar with CalendarId = " . $calendarId . " !!!!!! <br>";
@@ -504,7 +505,7 @@ function createItem($calendarId, $createdBy, $name, $note, $reminder, $type, $co
 	$note = trim($note);
 
 	$item_stmt = mysqli_prepare($conn, "INSERT INTO Items VALUES(0, ?, ?, NOW(), ?, ?, ?, ?, ?, ?);");
-	mysqli_stmt_bind_param($item_stmt, "issssiis", $calendarId, $name, $note, $reminder, $type, $createdBy, $location, $colour);
+	mysqli_stmt_bind_param($item_stmt, "issssiss", $calendarId, $name, $note, $reminder, $type, $createdBy, $location, $colour);
 	mysqli_stmt_execute($item_stmt);
 
 	$item_affected_rows = mysqli_stmt_affected_rows($item_stmt);
@@ -775,84 +776,129 @@ function getMinMaxItemsPerAccount($operation){
 // .............................................................................
 
 /**
+ *  !!!!!! TO DO: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *         Revert it back to binding + have check that type and itemId match. 
+ *  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *
  *	Edit an Item and its associated Event/Task if the item type is "event" or "task"
-  *	@param integer $itemId 		the item id
- *	@param string $name     	the item's title
- * 	@param string $note   		the item's description
- *	@param string $reminder 	the item's reminder datetime string
- *  @param string $type 		the item's type (one of "event", "task", "reminder", "note")
- *  @param array $options 		the item's additional information (start_date and end_date for 
- *								event, due_date and completion_date for task)
+ *  @param integer $accountId   the account id, REQUIRED
+ *  @param integer $calendarId  the calendar id, REQUIRED
+ *	@param integer $itemId 		the item id, REQUIRED
+ *  @param string $type 		the item's type (one of "event", "task", "reminder", "note"), REQUIRED
+ *	@param string $name     	the item's title, NULLABLE
+ * 	@param string $note   		the item's description, NULLABLE
+ *	@param string $reminder 	the item's reminder datetime string, NULLABLE
+ *  @param array $args 		    the item's additional information (start_date and end_date for 
+ *								event, due_date and completion_date for task), NULLABLE.
+ *                              if null, set it to array()
+ *  @param string $location     the item's location, NULLABLE
+ *  @param integer $colour 		the item's colour value (0-7), NULLABLE
  * 	@return boolean				true if item is edited successfully
  */
-function editItem($itemId, $name, $note, $reminder, $type, $options) {
-	// TODO: can refactor createItem and editItem
-	global $conn;
+function editItem($accountId, $calendarId, $itemId, $type, $name, $note, $reminder, $location, $colour, array $args = array()){
 
-	$item_edited = false; // return value
+    if (hasCalendarPermission($accountId, $calendarId, 'update') && 
+			$accountId && $calendarId && $itemId && $type){
 
-	// Fix input
-	$name = trim($name);
-	$note = trim($note);
+        global $conn;
+    	//print_r($args);
+        //echo "<br> ******* Editing item with Id = " . $itemId . " ******* <br>";
 
-	$item_stmt = mysqli_prepare($conn, "UPDATE Items SET name=?, note=?, reminder=? WHERE itemId=?");
-	mysqli_stmt_bind_param($item_stmt, "sssi", $name, $note, $reminder, $itemId);
-	mysqli_stmt_execute($item_stmt);
+    	// Checking for query onto Items -----------------------------------------
 
-	$item_affected_rows = mysqli_stmt_affected_rows($item_stmt);
-	mysqli_stmt_close($item_stmt); // close statement
-	// echo $item_affected_rows;
+        $query = "UPDATE Items SET ";
+        $params = array();
+        
+        if ($name){
+        	array_push($params, "name = '$name'");
+        }
 
-	$item_edited = $item_edited || ($item_affected_rows > 0);
+        if ($note){
+        	array_push($params, "note = '$note'");
+        }
 
-	// Handle edit Item Subclass (Event/Task)
-	echo $type;
-	$affected_rows = 0;
-	switch($type) {
-		case "event":
-			$start_date = $options["start_date"];
-			$end_date = $options["end_date"];
+        if ($reminder){
+        	array_push($params, "reminder = '$reminder'");
+        }
 
-			$stmt = mysqli_prepare($conn, "UPDATE EventItems SET startDate=?, endDate=? WHERE itemId=?");
-			mysqli_stmt_bind_param($stmt, "ssi", $start_date, $end_date, $itemId);
-			mysqli_stmt_execute($stmt);
+        if ($location){
+        	array_push($params, "location = '$location'");
+        }
 
-			$affected_rows = mysqli_stmt_affected_rows($stmt);
+        // Note that colour will ALWAYS be a param,
+        // so even if there is no change, it will trigger
+        // the query. 
+        if ($colour){
+        	array_push($params, "colour = '$colour'");
+        }
 
-			mysqli_stmt_close($stmt); // close statement
+      	if (!empty($params)){
+	        // Adding commas in the right places:
+	        for ($i = 0; $i < count($params)-1; $i++){
+	            $query .= $params[$i] . ", ";
+	        }
+	        $query .= $params[count($params)-1] . " WHERE itemId = $itemId;";
 
-			break;
-		case "task":
-			$due_date = $options['due_date'];
-			$completion_date = $options['completion_date'];
-			echo "got here";
+	        //echo("RESULTING QUERY: " . $query); 
+	        mysqli_query($conn, $query);
+			//echo(mysqli_error($conn));
 
-			$stmt = mysqli_prepare($conn, "UPDATE TaskItems SET dueDate=?, completionDate=? WHERE itemId=?");
-			mysqli_stmt_bind_param($stmt, "ssi", $due_date, $completion_date, $itemId);
-			// echo var_dump($stmt);
-			mysqli_stmt_execute($stmt);
+	        $query_success = checkUpdateSuccess($conn, $query);
 
-			$affected_rows = mysqli_stmt_affected_rows($stmt);
+	        if (!$query_success) {return false;}
+	    } // end if(!empty($params))
 
-			mysqli_stmt_close($stmt); // close statement
+	    // Checking for query onto EventItems/TaskItems --------------------------
 
-			break;
-		default: 
-			// if item is a reminder or note, then return here
-			return $item_edited;
-	}
+        if ($type){
 
-	// this is for testing only, to be deleted -----------------
-	if ($affected_rows > 0) {
-		echo "Edited Event or Task";
-	} else {
-		echo "Cannot edit Event or Task";
-	}
-	// ---------------------------------------------------------
+        	$type_params = array();
 
-	$item_edited = $item_edited || ($affected_rows > 0);
+	        if ($type == 'event'){
+	        	$type_query = "UPDATE EventItems SET ";
+		        if (isset($args['startDate'])){
+		            $startDate = $args['startDate'];
+		            array_push($type_params, "startDate = '$startDate'");
+		        }
 
-	return $item_edited;
+		        if (isset($args['endDate'])){
+		            $endDate = $args['endDate'];
+		            array_push($type_params, "endDate = '$endDate'");
+		        }
+	    	}
+	    	else if ($type == 'task'){
+	    		$type_query = "UPDATE TaskItems SET ";
+	    		if (isset($args['dueDate'])){
+		            $dueDate = $args['dueDate'];
+		            array_push($type_params, "dueDate = '$dueDate'");
+		        }
+
+		        if (isset($args['completionDate'])){
+		            $completionDate = $args['completionDate'];
+		            array_push($type_params, "completionDate = '$completionDate'");
+		        }
+	    	}
+
+	    	if (!empty($type_params)){
+		    	//Adding commas in the right places:
+		        for ($i = 0; $i < count($type_params)-1; $i++){
+		            $type_query .= $type_params[$i] . ", ";
+		        }
+		        $type_query .= $type_params[count($type_params)-1] . " WHERE itemId = $itemId;";
+
+		        //echo("RESULTING QUERY: " . $type_query); 
+		        mysqli_query($conn, $type_query);
+
+		        return checkUpdateSuccess($conn, $type_query);
+		    }
+	    	else {return true;} // end if($type_query)
+	    }
+	    else {return true;} // end if($type)
+    }
+    else{
+        echo "<br> !!!!!! COULD NOT Update Item with ItemId = " . $itemId . " !!!!!! <br>";
+        return false;
+    }
 }
 
 // .............................................................................
@@ -875,7 +921,7 @@ function deleteItem($itemId) {
 
 	mysqli_stmt_close($stmt); // close statement
 
-	return $affected_rows > 0;
+	return checkUpdateSuccessWithStmt(($affected_rows > 0), 'DELETE FROM Items Where itemId=$itemId');
 }
 
 // =============================================================================
@@ -1093,7 +1139,7 @@ function updateAccount($accountId, array $args = array()){
     echo("RESULTING QUERY: " . $query); 
     mysqli_query($conn, $query);
 
-    return checkUpdateSuccess($conn);
+    return checkUpdateSuccess($conn, $query);
 }
 
 // .............................................................................
@@ -1133,8 +1179,8 @@ function hasCalendarPermission($accountId, $calendarId, $operationType){
         $permissionType = mysqli_fetch_assoc($response)["permissionType"];
 
         if (in_array($permissionType, $operationPermission[$operationType])){
-            echo "<br> [Permission Check: AccountId " . $accountId . " has permission to 
-            modify Calendar with id = " . $calendarId . ".]<br>";
+            // echo "<br> [Permission Check: AccountId " . $accountId . " has permission to 
+            // modify Calendar with id = " . $calendarId . ".]<br>";
 
             return true;
         }
@@ -1151,16 +1197,69 @@ function hasCalendarPermission($accountId, $calendarId, $operationType){
 // Checks whether an update to the DB was successful.
 // ___ RETURNS: true if update was successful,
 //              otherwise false.
-function checkUpdateSuccess($conn){
+function checkUpdateSuccess($conn, $query){
 
-    if (mysqli_affected_rows($conn) > 0) {
-        echo "<br> Record updated successfully. Rows affected: " . mysqli_affected_rows($conn) . "<br>";
+    if (mysqli_affected_rows($conn) >= 0) {
+        //echo "<br> No errors in record update. Rows affected: " . mysqli_affected_rows($conn) . "<br>";
+        $affected_rows = mysqli_affected_rows($conn);
+        // Alert:
+		echo <<<_END
+			<div class="positive_sql_alert">
+			  	<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+			  	<strong>No errors in record update.</strong> 
+			  	<div style="margin-left: 15px">
+			  	Rows affected: $affected_rows
+			  	<br> Query: $query
+			  	</div>
+			</div>
+_END;
+
         return true;
         } 
     else {
-        echo "<br> !!!! Record not updated. No rows affected. Check query. <br>";
+        //echo "<br> !!!! Record not updated. No rows affected. Check query. <br>";
+       	// Alert:
+		echo <<<_END
+			<div class="negative_alert">
+			  	<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+			  	<strong>Errors in record update.</strong>
+			  	<div style="margin-left: 15px">
+			  	Query: $query
+			  	</div>
+			</div>
+_END;
+
         return false;
     }
+}
+
+function checkUpdateSuccessWithStmt($row_is_affected, $query){
+	if ($row_is_affected){
+		// Alert:
+		echo <<<_END
+			<div class="positive_sql_alert">
+			  	<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+			  	<strong>No errors in record update.</strong> 
+			  	<div style="margin-left: 15px">
+				Query: $query
+			  	</div>
+			</div>
+_END;
+
+        return true;
+	}
+	else{
+		// Alert:
+		echo <<<_END
+			<div class="negative_alert">
+			  	<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+			  	<strong>Errors in record update.</strong>
+			  	<div style="margin-left: 15px">
+			  	Query: $query
+			  	</div>
+			</div>
+_END;
+	}
 }
 
 ?>
